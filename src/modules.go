fe7547953ef48ea2890099fc3269ebd61c674b1a
LukasDrsman <lukaskodr@gmail.com>
2022-03-05 21:45:41 +0100

    Rewrite run1 into general Module form

--- src/modules.go
+++ src/modules.go
@@ -1,24 +1,6 @@
 package main
 
 /*
-    TODO:
-        [ ] implement modules (OOP)
-
-    STRUCTURE:
-
-    type ModuleName struct {
-        shifter Shifter
-
-        field ...
-    }
-
-    func (this ModuleName) FunctionName(args... type) {
-        this.shifter.BeginShifting(ID)
-        this.shifter.AwaitShifting()
-
-        implementation ...
-    }
-
     USEFUL FUNCTIONS:
         @ shifter.go
             - (Shifter).BeginShifting(id)
@@ -26,89 +8,6 @@ package main
             - (Shifter).DriveAbsolute(target, rate)
             - (Shifter).DriveRelative(target, rate)
 */
-// type M1S0 struct {
-//     shifter Shifter
-// }
-//
-// func NewM1S0(shifter Shifter) M1S0 {
-//     return M1S0 {
-//         shifter: shifter,
-//     }
-// }
-//
-// func (this M1S0) Function() {
-//     this.shifter.BeginShifting(0)
-//     this.shifter.AwaitShifting()
-//     this.shifter.DriveRelative(530, 400)
-// }
-
-type M1S0 struct {
-    shifter Shifter
-}
-
-func NewM1S0(shifter Shifter) M1S0 {
-    return M1S0 {
-        shifter: shifter,
-    }
-}
-
-func (this M1S0) Extend() {
-    this.shifter.BeginShifting(0)
-    this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(-1700, 500)
-}
-
-func (this M1S0) ParitalExtend() {
-    this.shifter.BeginShifting(0)
-    this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(-400, 500)
-}
-
-func (this M1S0) Retract() {
-    this.shifter.BeginShifting(0)
-    this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(1700, 500)
-}
-
-func (this M1S0) BeginRetract() {
-    this.shifter.BeginDriveRelative(500, 500)
-}
-
-func (this M1S0) AwaitRetract() {
-    this.shifter.AwaitDriveRelative()
-}
-
-type M1S1 struct {
-    shifter Shifter
-}
-
-func NewM1S1(shifter Shifter) M1S1 {
-    return M1S1 {
-        shifter: shifter,
-    }
-}
-
-func (this M1S1) Flip() {
-    this.shifter.BeginShifting(1)
-    this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(900, 500)
-}
-
-type M1S3 struct {
-    shifter Shifter
-}
-
-func NewM1S3(shifter Shifter) M1S3 {
-    return M1S3 {
-        shifter: shifter,
-    }
-}
-
-func (this M1S3) Open() {
-    this.shifter.BeginShifting(3)
-    this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(600, 500)
-}
 
 type Module struct {
     shifter Shifter
@@ -123,13 +22,13 @@ func NewModule(shifter Shifter) Module {
 func (this Module) Drive(target, id int) {
     this.shifter.BeginShifting(id)
     this.shifter.AwaitShifting()
-    this.shifter.DriveRelative(target, 400)
+    this.shifter.DriveRelative(target, 500)
 }
 
 func (this Module) BeginDrive(target, id int) {
     this.shifter.BeginShifting(id)
     this.shifter.AwaitShifting()
-    this.shifter.BeginDriveRelative(target, 400)
+    this.shifter.BeginDriveRelative(target, 500)
 }
 
 func (this Module) AwaitDrive() {
--- src/run1.go
+++ src/run1.go
@@ -11,10 +11,7 @@ func main() {
 	sens2 := NewSensing("4")
 	shift := NewShifter("B", "C")
 
-	// modules
-	miso := NewM1S0(shift)
-	misi := NewM1S1(shift)
-	mise := NewM1S3(shift)
+	module := NewModule(shift)
 
 	p1 := sens2.ColorCalib("purple")
 	p2 := sens1.ColorCalib("white")
@@ -24,33 +21,31 @@ func main() {
 	base.Move(400, sens2, pid, p1)
 	base.RotateR(-42, 200)
 
-	miso.BeginRetract()
+	module.BeginDrive(500, 0)	// async retraction
 
 	base.Move(100, sens1, pid, p2)
 
-	miso.AwaitRetract()
-	miso.Extend()
-	miso.Retract()
+	module.AwaitDrive()			// await retraction
+	module.Drive(-1700, 0)		// extend
+	module.Drive( 1700, 0)		// retract
 
-	shift.BeginShifting(1) // shift to M1S1
-
+	shift.BeginShifting(1)		// async shift to module 1
 	base.Rotate(12, 200)
 	base.MoveTimed(300, 0.39, pid)
 	base.Rotate(-12, 200)
 
-	misi.Flip()
+	module.Drive(900, 1)
+
 	base.Rotate(12, 200)
-	shift.BeginShifting(0); shift.AwaitShifting()
 
-	miso.Extend()
+	module.Drive(-1700, 0)		// extend
 
-	shift.BeginShifting(3)
-
+	shift.BeginShifting(3)		// async shift to module 3
 	base.MoveTimedReverse(300, 0.61, pid)
 
-	mise.Open()
+	module.Drive(600, 3)		// open
 
+	shift.BeginShifting(0)		// async shift to module <0, next run>
 	time.Sleep(2 * time.Second)
-
 	base.MoveTimedReverse(500, 2.0, pid)
 }
