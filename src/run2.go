62b826167aa17931500c02002adca154408a20e0
LukasDrsman <lukaskodr@gmail.com>
2022-04-20 18:53:03 +0200

    Incomplete commit, 20th April changes

--- src/run2.go
+++ src/run2.go
@@ -3,15 +3,16 @@ package main
 
 func main() {
 	pid   := NewPID(0.0, 25.0, 25.0, 0.0)
-	pid15 := NewPID(15.0, 5.0, 5.0, 0.25)
-    // pidns := NewPIDNS(pid)
-	i := NewPID(0.0, 1.0, 10.0, 0.0)
+	null  := NewPID(0.0, 0, 0, 0.0)
+	// pid15 := NewPID(15.0, 5.0, 5.0, 0.25)
+    pidns := NewPIDNS(pid)
+	// i := NewPID(0.0, 1.0, 10.0, 0.0)
 	// ins := NewPIDNS(i)
 	base := NewBase("A", "D", "1")
 
 	// sensL := NewSensing("3")
     sensB := NewSensing("2")
-	// sensR := NewSensing("4")
+	sensR := NewSensing("4")
 
 	shift := NewShifter("B", "C")
 	module := NewModule(shift)
@@ -19,19 +20,21 @@ func main() {
 	// colors
 	// drabB := sensB.ColorCalib("drabB")
 	// blackB := sensB.ColorCalib("blackB")
-	whiteB := sensB.ColorCalib("whiteB")
-	// blueB  := sensB.ColorCalib("blueB")
+	// whiteB := sensB.ColorCalib("whiteB")
+	blueB  := sensB.ColorCalib("blueB")
 	//
 	// greyL := sensL.ColorCalib("greyL")
 	// greenL := sensL.ColorCalib("greenL")
 	// whiteL := sensL.ColorCalib("whiteL")
 	//
 	// blackR := sensR.ColorCalib("blackR")
+	whiteR := sensR.ColorCalib("whiteR")
 
 
 	// run
 	AwaitButton()
 
+	// SECTOR 1
 	// base.MoveTimedNS(300, 0.5, &pidns, _initial)
 	// base.MoveNS(300, sensB, &pidns, drabB, _initial)
 	// base.MoveNS(300, sensB, &pidns, whiteB, _final)
@@ -49,23 +52,34 @@ func main() {
 	// module.Drive(-600, 0)
 	// module.Drive(4*360 + 600, 0)
 	//
+
+	// SECTOR 2
 	shift.BeginShifting(2)
-	base.Rotate(-15, 100)
-	base.MoveTimed(300, 2.0, pid15)
+	base.MoveTimedNS(300, 0.6, &pidns, _initial)
+	base.MoveNS(300, sensR, &pidns, whiteR, _final)
 
 	// base.MoveTimed(300, 0.1, pid)
 	module.Drive(-4*360, 2)
 
 	shift.BeginShifting(3)
-	base.Rotate(-10, 100)
+	// base.Rotate(-10, 100)
 
 	// base.MoveReverseNS(300, sensB, &pidns, blackB, _initial)
 	// base.MoveReverseNS(300, sensB, &pidns, whiteB, _midrun)
-	base.MoveTimedReverse(300, 0.8, pid)
+	base.MoveTimedReverse(200, 1.35, pid)
 
-	base.RotateL(100, 300)
+	base.RotateL(90, 300)
 	module.Drive(1.5*360, 3)
-	base.MoveReverse(300, sensB, i, whiteB)
-	base.MoveTimed(300, 0.1, i)
-	module.Drive(-1.5*360, 3)
+	base.MoveTimedReverse(300, 2.0, null)
+	base.MoveTimed(300, 0.1, pid)
+	module.Drive(-2.25*360, 3)
+
+	// SECTOR 3
+	shift.BeginShifting(1)
+	base.RotateR( 90, 200)
+	module.Drive(-8*360, 1)
+	module.Drive( 8*360, 1)
+	shift.BeginShifting(0)
+	base.RotateR(-90, 200)
+	base.Move(300, sensR, pid, blueB)
 }
