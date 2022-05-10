651a6311abc8a86a6814b4690ed59e9fd3ec00b6
Ondrisko <ondro.3.3.3.b@gmail.com>
2022-05-10 16:21:53 +0200

    Changes to run

--- src/run.c
+++ src/run.c
@@ -14,7 +14,7 @@ int main(void)
     color cs_f, cs_s;
 
     b = baseNew('D', 'A', '1');
-    ctl = pidNew(0.0, 20.0, 0.1, 50.0);
+    ctl = pidNew(0.0, 25.0, 25.0, 0.0);
     s = shifterNew('B', 'C');
     cs_f = colorNew('4');
     cs_s = colorNew('3');
@@ -23,7 +23,14 @@ int main(void)
     colorProfileLoad(&cs_s, "../data/profile_k_S", "../data/profile_l_S");
 
     getc(stdin);
+
+    moveTimed(b, 600, 1.5, &ctl, FWD, NS_STD);
+
     moveLine(b, 600, cs_f, cs_s, 100.0, 7.0, -41, FWD, LBRW, NS_STD);
+
+    moduleDrive(s, -450, 1);
+
+    moduleDrive(s, 400, 3);
 
     return 0;
 }
